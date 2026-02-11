88aa6ecda96f7053036a84ca094be80cd61e5584
Ryan Pugh <rpugh777@yahoo.com>
2026-02-10 20:28:06 -0800

    removed Intent from the methods as they were doing nothing, added adding a new num

--- app/src/main/java/com/example/midtermpractice/MainActivity2.java
+++ app/src/main/java/com/example/midtermpractice/MainActivity2.java
@@ -21,6 +21,8 @@ public class MainActivity2 extends AppCompatActivity {
     Button cube;
     Button divisors;
 
+    Button newNum;
+
     TextView output;
 
     int[] multi;
@@ -41,13 +43,13 @@ public class MainActivity2 extends AppCompatActivity {
         square = findViewById(R.id.button3);
         cube = findViewById(R.id.button4);
         divisors = findViewById(R.id.button5);
+        newNum = findViewById(R.id.button8);
         output = findViewById(R.id.textView);
         Intent intent = getIntent();
         number = intent.getIntExtra("number", 0);
         multiples.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i2 = new Intent(MainActivity2.this, MainActivity.class);
                 multi = getMuti(number);
                 String result = "";
                 for(int i = 1; i < multi.length; i++){
@@ -61,7 +63,6 @@ public class MainActivity2 extends AppCompatActivity {
         square.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i2 = new Intent(MainActivity2.this, MainActivity.class);
                 int square = getSquare(number);
                 String result = String.valueOf(square);
                 output.setText(result);
@@ -71,7 +72,6 @@ public class MainActivity2 extends AppCompatActivity {
         cube.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i2 = new Intent(MainActivity2.this, MainActivity.class);
                 int cube = getCube(number);
                 String result = String.valueOf(cube);
                 output.setText(result);
@@ -81,7 +81,6 @@ public class MainActivity2 extends AppCompatActivity {
         divisors.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i2 = new Intent(MainActivity2.this, MainActivity.class);
                 int[] div = getDivisors(number);
                 String result = "";
                 for(int i = 0; i < div.length; i++){
@@ -91,6 +90,14 @@ public class MainActivity2 extends AppCompatActivity {
                     }
                 }
                 output.setText(result);
+            }
+        });
+
+        newNum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i2 = new Intent(MainActivity2.this, MainActivity.class);
+                startActivity(i2);
             }
         });
     }
